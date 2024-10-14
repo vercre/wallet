e666bcea7cf00d619fa61f070b70a09fb7f9985a
Andrew Goldie <andrew.goldie@goldiesystems.com>
2024-10-14 16:14:23 +1300

    representative crux app

---
+++ wallet/README.md
@@ -0,0 +1,3 @@
+# Vercre-Wallet
+
+This crate is the shared Crux-based application upon which the various platform-specific shells are built. See the workspace README for more information.
---
+++ wallet/src/lib.rs
@@ -0,0 +1,36 @@
+// The following is needed for uniffi generated code to pass linting.
+#![allow(clippy::empty_line_after_doc_comments)]
+
+pub mod app;
+pub mod capabilities;
+
+use lazy_static::lazy_static;
+use wasm_bindgen::prelude::wasm_bindgen;
+
+pub use crux_core::bridge::{Bridge, Request};
+pub use crux_core::Core;
+pub use crux_http as http;
+
+pub use app::*;
+pub use capabilities::sse;
+
+uniffi::include_scaffolding!("wallet");
+
+lazy_static! {
+    static ref CORE: Bridge<Effect, App> = Bridge::new(Core::new());
+}
+
+#[wasm_bindgen]
+pub fn process_event(data: &[u8]) -> Vec<u8> {
+    CORE.process_event(data)
+}
+
+#[wasm_bindgen]
+pub fn handle_response(id: u32, data: &[u8]) -> Vec<u8> {
+    CORE.handle_response(id, data)
+}
+
+#[wasm_bindgen]
+pub fn view() -> Vec<u8> {
+    CORE.view()
+}
